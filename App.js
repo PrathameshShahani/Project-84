ea60cab884067cd2be97c3560c3409f405bdb986
PrathameshShahani <umashahani5@gmail.com>
2021-08-29 13:14:05 +0530

    added logout and switch

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import DrawerNavigator from './navigation/DrawerNavigation';
+import * as React from "react";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
-export default class App extends React.Component{
-  render(){
-    return(
-      <NavigationContainer>
-        <DrawerNavigator/>
-      </NavigationContainer>
-    )
-  }
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import DashboardScreen from "./screens/DashboardScreen";
+
+import * as firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+});
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+export default function App() {
+  return <AppNavigator />;
 }
